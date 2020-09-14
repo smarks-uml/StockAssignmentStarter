2fe3216811fd5e3ca0b4cc33618c92091f85eb00
spencer.marks <smarks@origamisoftware.com>
2020-09-14 12:00:14 -0400

    initial commit

---
+++ src/main/java/edu/uml/studentname/stocktrader/StockTraderApp.java
@@ -0,0 +1,11 @@
+package edu.uml.studentname.stocktrader;
+
+/**
+ * Client code for the StockService application.
+ */
+public class StockTraderApp {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
